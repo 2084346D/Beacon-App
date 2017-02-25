dda33b1c82c8bb1d9e80e7504b001695465a5053
Hilmi <hilmimarzuqi@gmail.com>
2017-02-25 18:34:53 +0000

    changed ui for pages, added buttons and textfield

--- ShoppingBeacon/app/src/main/java/com/example/shoppingbeacon/AddActivity.java
+++ ShoppingBeacon/app/src/main/java/com/example/shoppingbeacon/AddActivity.java
@@ -1,16 +1,50 @@
 package com.example.shoppingbeacon;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class AddActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        // list elements added in this array
+        String[] StringArray = {"Eggs", "Bread"};
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.add_listview, StringArray);
+
+        ListView listView = (ListView) findViewById(R.id.shopping_list);
+        listView.setAdapter(adapter);
     }
 }
